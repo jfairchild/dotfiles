8374c0782b19499a1a14b36c4169b1151444e1b8
Nick Lopez <nlopez@gmail.com>
2011-09-08 19:53:30 -0400

    allows bootstrap.sh to run from everywhere

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
+cd $(dirname ${0})
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av $(basename ${0}) ~
 fi
 echo
