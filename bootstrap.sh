e369cc8e87779dd3acd46e6b82ab9febb1916b04
Nick Lopez <nlopez@gmail.com>
2011-09-09 01:47:37 -0400

    cleanup bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,9 @@
 #Potentially dangerous...
 export GIT_SSL_NO_VERIFY=true
 
-DIR=$(dirname $0)
-cd $DIR
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av $DIR/ ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 fi
 echo
