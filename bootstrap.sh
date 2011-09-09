88465e53fb59bb85fd5d14ff8aec9bbb5dabb72e
Nick Lopez <nlopez@gmail.com>
2011-09-08 20:07:27 -0400

    fixes cwd bug in bootstrap. now properly runs no matter where you call it from.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
-cd $(dirname ${0})
+#!/bin/bash -eu
+DIR=$(dirname $0)
+cd $DIR
 git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av $(basename ${0}) ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av $DIR/ ~
 fi
 echo
