e321c04e17a10cc3bc055f37b7773e2af007b359
Nick Lopez <nlopez@gmail.com>
2011-09-20 17:12:13 -0400

    moves submodule update from instructions to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@
 export GIT_SSL_NO_VERIFY=true
 
 git pull
+git submodule init
+git submodule update
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
